e60bea1086b303442b9716402373aa36449b2745
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-25 15:01:36 +0100

    save changes

---
+++ 5-Conditional_Statements/taak05 - switch conditional/index.php
@@ -0,0 +1,18 @@
+<?php
+$score = 10;
+
+switch($score) {
+    case 10:
+    echo "Wow wat een fantastische score!"."<br>";
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!"."<br>";
+    case 8:
+    echo "Goed bezig, bijna alle punten!"."<br>";
+    case 7:
+    echo "Ga zo door!"."<br>";
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!"."<br>";
+}
+
+
+?>
