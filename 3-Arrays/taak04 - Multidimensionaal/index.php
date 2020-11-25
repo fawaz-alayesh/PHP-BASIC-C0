678f85d2497778df62b99b3b362827d2e06f44fa
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-25 14:25:01 +0100

    save changes

---
+++ 3-Arrays/taak04 - Multidimensionaal/index.php
@@ -0,0 +1,11 @@
+<?php
+$abcGetallen=[["a","b","c","d"],[24,56,78,90]];
+print_r($abcGetallen);
+echo"<br>";
+echo $abcGetallen[0][0];
+echo $abcGetallen[1][1];
+echo "<br>";
+echo $abcGetallen[0][2];
+echo $abcGetallen[1][3];
+
+?>
